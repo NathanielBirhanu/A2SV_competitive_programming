0296e784a50a864fb12b7ebf6ca2a51b481da1e8
NathanielBirhanu <136117245+NathanielBirhanu@users.noreply.github.com>
2024-04-11 00:40:19 +0300

    Create add_space.py

---
+++ add_space.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        resArr = []
+        i = 0
+        
+		# Take portions of the string between spaces and put them in the list
+        for index in spaces:
+            portion = s[i:index]
+            resArr.append(portion)
+            i = index
+            
+        #Put the remaining string in resArr
+        resArr.append(s[spaces[len(spaces) - 1]:])
+        return " ".join(resArr)
+            
