588d057691a189ec955939a05656ee6d46aa98b6
NathanielBirhanu <136117245+NathanielBirhanu@users.noreply.github.com>
2024-05-08 08:10:19 +0300

    Create remove_duplicate.py

---
+++ remove_duplicate.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        used = set()
+        c = [0] * 26
+        a = ord('a')
+        for char in s:
+            c[ord(char) - a] += 1
+        ans = []
+        for char in s:
+            if len(ans) == 0:
+                used.add(char)
+                ans.append(char)
+            elif char not in used and char > ans[-1]:
+                used.add(char)
+                ans.append(char)
+            elif char not in used:
+                while ans and ans[-1] >= char and c[ord(ans[-1]) - a] > 0:
+                    delete_char = ans.pop()
+                    used.discard(delete_char)
+                used.add(char)
+                ans.append(char)
+            c[ord(char) - a] -= 1
+        return ''.join(ans)
